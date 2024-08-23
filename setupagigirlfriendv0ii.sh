5756e864259850389fd206236bf670adab298a6b
Montreal.AI <info@montreal.ai>
2024-08-23 10:23:47 -0400

    Add files via upload

--- setupagigirlfriendv0ii.sh
+++ setupagigirlfriendv0ii.sh
@@ -90,7 +90,7 @@ def chat():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5004)
+    app.run(debug=True, port=5000)
 EOF
 
 # Create index.html
